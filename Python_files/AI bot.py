7a52101a63f14e78a0ac6ff5f971b5eb9f37e057
n1k <nik.borgoyakov@bk.ru>
2024-10-22 16:57:50 +0700

    COMMIT AI BOT START 22.10.24

--- Python_files/AI bot.py
+++ Python_files/AI bot.py
@@ -1,3 +1,12 @@
-import asyncio
-import aiogram
+# начало разработки AI bot
+from asyncio import sleep
+from aiohttp import ClientSession, Bot
+text = 'Здравствуйте, Я умный бот. Если вы чувствуете, что иногда тратите свое время впустую, то я могу вам помочь. Выберите интересующие вас теги: '
+
+# здесь надо определиться с тегами
+parameters = []
+
+
+
+
 
