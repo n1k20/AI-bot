a1a0b26426877557ed40b50c8fe5f1d2cf60b192
n1k <nik.borgoyakov@bk.ru>
2024-11-08 07:44:14 +0700

    COMMIT n1k command_start and echo

--- Python_files/AI bot.py
+++ Python_files/AI bot.py
@@ -1,3 +1,33 @@
 import asyncio
-import aiogram
 
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+
+from config import TOKEN
+
+bot = Bot(token=TOKEN)
+dispatcher = Dispatcher()
+
+
+@dispatcher.message(CommandStart())
+async def cmd_start(message: types.Message) -> None:
+    await message.answer('Hello')
+
+
+@dispatcher.message()
+async def echo(message: types.Message) -> None:
+    user_words = message.text.lower().split()
+    answer_one = ['hello', 'привет', 'good day', 'hi', 'здравствуйте']
+    if any(word in answer_one for word in user_words):
+        await message.answer('Приветствую вас в моем чате. AI bot !!!!')
+    elif len(user_words[0]) > 0:
+        await message.answer('Ты че не здароваешься')
+    else:
+        await message.answer(message.text)
+
+
+async def main() -> None:
+    await dispatcher.start_polling(bot)
+
+
+asyncio.run(main())
