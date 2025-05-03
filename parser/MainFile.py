6e888fb0245b979e47fe688c905d9ca95fdea239
n1k17 <nik.borgoyakov@bk.ru>
2025-05-03 23:13:18 +0700

    merge

--- parser/MainFile.py
+++ parser/MainFile.py
@@ -1,19 +1,11 @@
 import asyncio
 
 from telethon import TelegramClient
-
-API_ID = 25113344
-API_HASH = "69e2e4cf228276b6f774da84716cc57a"
+from config import API_ID, API_HASH
 
 CLIENT = TelegramClient('session_name', API_ID, API_HASH)
 CLIENT.start()
-
-CHANNEL_USERNAME = "https://t.me/premium"
-
-
-async def get_messages(channel_username: str) -> None:
+async def get_messages(channel_username: str, api_hash: str, api_id: int) -> None:
     async for message in CLIENT.iter_messages(channel_username, limit=10):
         print(message.text)
 
-
-asyncio.run(get_messages(channel_username=CHANNEL_USERNAME))
